08e90582a2c203e35cf2bd69988ae3187814b63f
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2026-07-09 09:50:20 -0300

    Implementação até a aula 5

--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@ Execute: npm install express --save
 
 Para que as alterações subam sem precisar reinicializar o servidor.
 npm install nodemon -D (-D indica que só vai funcionar em desenvolvimento, não vai subir para a produção.)
+
+Para criar um script é acessado o arquivo package.json, na chave script. E para executar execute: npm run dev (dev é a chave do script criado.)
--- src/app.js
+++ src/app.js
@@ -1,16 +1,62 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
-const port = 3000
+
+// indicar para o express ler o body com json
+app.use(express.json())
+
+// mock
+const selecoes = [
+    {id: 1 , selecao: 'Brasil', grupo:'G'},
+    {id: 2 , selecao: 'Suíça', grupo:'G'},
+    {id: 3 , selecao: 'Sérvia', grupo:'G'},
+    {id: 4 , selecao: 'Camarões', grupo:'G'}    
+]
+
+function buscarSelecaoporId(id){
+    return selecoes.find(selecao => selecao.id == Number(id))
+}
+
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id === Number(id))
+}
 
 
 // criar rota padrão ou raiz
-
 app.get('/',(req, res) => {
-    res.send('Hello World!')
+    res.send('Curso de Node JS')
 })
 
-// escutar a porta
-
-app.listen(port, () => {
-    console.log(`Servidor rodando no enderenço http://localhost:${port}`)
+app.get('/selecoes',(req, res) => {
+    res.status(200).send(selecoes)
 })
+
+app.get('/selecoes/:id', (req, res) => {
+    const objeto = buscarSelecaoporId(req.params.id)
+    if (!objeto) {
+        return res.status(404).json({erro: "Seleção não encontrada!"})
+    } 
+        
+    return res.status(200).json(objeto)        
+})
+
+app.post('/selecoes',(req, res) => {
+    const novaSelecao = req.body
+    selecoes.push(novaSelecao)
+    res.status(201).json({
+        mensagem: 'Seleção cadastrada com sucesso!', 
+        dados: novaSelecao
+    })
+})
+
+app.delete('/selecoes/:id', (req,res) => {
+    const index = buscarIndexSelecao(req.params.id)
+    if (index < 0) {
+        return res.status(404).json({erro:`Seleção com o id ${req.params.id} não encontrada!`})
+    } 
+    
+    selecoes.splice(index,1)
+    return res.status(200).send(`Seleção com o id ${req.params.id} excluída com sucesso!`)
+})
+
+export default app
